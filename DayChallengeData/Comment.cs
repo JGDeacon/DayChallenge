80b8b5ac8907080a4ef110d054d6ced03222ec43
JGDeacon <deacon6878@gmail.com>
2021-04-13 10:04:58 -0400

    Comment Class Properties

--- DayChallengeData/Comment.cs
+++ DayChallengeData/Comment.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +10,13 @@ namespace DayChallengeData
 {
     public class Comment
     {
+        [Key]
+        public int CommentID { get; set; }
+        public string Text { get; set; }
+        public Guid AuthorID { get; set; }
+        public virtual List<Reply> Replies { get; set; }
+        //[ForeignKey(nameof(Reply))]
+        public int PostID { get; set; } //Needs to be FK after first migration
+        public virtual Reply Reply { get; set; }
     }
 }
