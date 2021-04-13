bcc1e87437146bcdf45620b43d9669b1322cf8e5
ShirishaB88 <shirisha46@gmail.com>
2021-04-13 16:10:06 -0400

    Adding Shirisha's Part

--- DayChallengeModels/PostListitems.cs
+++ DayChallengeModels/PostListitems.cs
@@ -1,26 +1,19 @@
-﻿using System;
+﻿using DayChallengeData;
+using System;
 using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace DayChallengeData
+namespace DayChallengeModels
 {
-
-    public class Post
+    public class PostListItem
     {
-        [Key]
         public int Id { get; set; }
-        [Required]
         public string Title { get; set; }
-        [Required]
         public string Text { get; set; }
-        [Required]
-        public Guid AuthorId { get; set; }
-
+        //public int CommentId { get; set; }
         public virtual Comment Comment { get; set; }
+
     }
 }
-
