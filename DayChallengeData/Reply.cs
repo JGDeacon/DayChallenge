fbf2bf56e5c35575db8014c66e1000d2455c50a7
aschump <schumpertamber@gmail.com>
2021-04-13 11:15:51 -0400

    completed model

--- DayChallengeData/Reply.cs
+++ DayChallengeData/Reply.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +9,13 @@ namespace DayChallengeData
 {
     public class Reply
     {
+        [Key]
+        public int ReplyId { get; set; }
+
+        [Required]
+        public string Text { get; set; }
+
+        [Required]
+        public Guid AuthorId { get; set; }
     }
 }
