a70eb47d5c52ea0ec860f6395bf02c58ab0c4a13
JGDeacon <deacon6878@gmail.com>
2021-04-13 09:52:04 -0400

    Dev Branch

--- DayChallengeData/Comment.cs
+++ DayChallengeData/Comment.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace DayChallengeData
 {
-    class Comment
+    public class Comment
     {
     }
 }
--- DayChallengeData/Post.cs
+++ DayChallengeData/Post.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace DayChallengeData
 {
-    class Post
+    public class Post
     {
     }
 }
